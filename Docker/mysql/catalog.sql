a4089c78fcea449f312eab6894ae86a67ef8779f
Arivan Amin <arivanamin@gmail.com>
2026-08-01 10:42:10 +0300

    Modify catelog database variables

--- Docker/mysql/catalog.sql
+++ Docker/mysql/catalog.sql
@@ -2,14 +2,14 @@ CREATE
 DATABASE IF NOT EXISTS catalog_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE
-USER IF NOT EXISTS 'catalog_user'@'%' IDENTIFIED BY 'catalog@@##'; GRANT SELECT, INSERT,
+USER IF NOT EXISTS 'catalog_user'@'%' IDENTIFIED BY 'catalog@@pass'; GRANT SELECT, INSERT,
 UPDATE,
 DELETE
 ON catalog_db.* TO 'catalog_user'@'%';
 
 -- Liquibase migration user
 CREATE
-USER IF NOT EXISTS 'catalog_migration'@'%' IDENTIFIED BY 'catalog##cmy';
+USER IF NOT EXISTS 'catalog_migration'@'%' IDENTIFIED BY 'catalog@@cmy';
 GRANT ALL PRIVILEGES ON catalog_db.* TO
 'catalog_migration'@'%';
 
